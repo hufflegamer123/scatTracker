21c3a4d374ce72ed699acada2781a31c6ff33e7b
Logan <hockeyplayerlogan@gmail.com>
2022-03-18 18:01:33 -0400

    hi

---
+++ scatter.js
@@ -0,0 +1,6 @@
+'use strict'
+
+let wordlist = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"];
+let scatCount = 5;
+let before = Math.floor(Math.random() * scatCount);
+let after = scatCount - before;
