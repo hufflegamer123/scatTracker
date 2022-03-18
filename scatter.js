9229405840d3cfb2b930be9de07314f2d2611bd9
Austin Kotlus <austin.kotlus@pdri.com>
2022-03-18 18:10:01 -0400

    testing plugin

--- scatter.js
+++ scatter.js
@@ -4,3 +4,8 @@ let wordlist = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", 
 let scatCount = 5;
 let before = Math.floor(Math.random() * scatCount);
 let after = scatCount - before;
+
+console.log('test');
+chrome.webRequest.onBeforeRequest.addListener(async (req) => {
+    console.log(`Before URL loaded: ${req.url}`);
+}, {urls: ['*://*/*']});
